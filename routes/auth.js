d1a7ba9a773b4ed240425593e8645b2278364a94
Himanshu Pal <mailtohimanshupal@gmail.com>
2021-07-06 06:25:19 +0530

    Removed node_modules, .env & Error handling

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,17 @@ const User= require("../models/User")
 //register
 
 router.get("/register", async (req,res)=>{
-    const user= await new User({
-        username:"john",
-        email:"dada@gmail.com",
-        password: "123456"
-    })
-await user.save();
-res.send("ok")
+    try {
+        const user = await new User({
+            username: "john",
+            email: "dada@gmail.com",
+            password: "123456"
+        })
+        const data = await user.save();
+        res.json(await data.json())
+    } catch (error) {
+        res.json({error: error.message}) // Client too should know of errors
+    }
 });
 
 
