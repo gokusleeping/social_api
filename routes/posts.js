99b4122fff237b7f2867d22ae6248dd3740d3ca0
Ranvir Singh <gokusleeping@gmail.com>
2021-07-30 00:05:10 +0530

    update

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Post = require("../models/Posts");
+const Post = require("../models/Post");
 const User = require("../models/User");
 
 //create a post
