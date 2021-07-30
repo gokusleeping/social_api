5da22ee960e6c70e6f24253eb12ce7a30b9f942e
Ranvir Singh <gokusleeping@gmail.com>
2021-07-31 00:57:27 +0530

    .

--- index.js
+++ index.js
@@ -12,39 +12,37 @@ const conversationRoute = require("./routes/conversations");
 const messageRoute = require("./routes/messages");
 const router = express.Router();
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+	console.log("Connected to MongoDB");
+});
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
+	destination: (req, file, cb) => {
+		cb(null, "public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	}
 });
 
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("File uploded successfully");
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		return res.status(200).json("File uploded successfully");
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 app.use("/api/auth", authRoute);
@@ -54,5 +52,5 @@ app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+	console.log("Backend server is running!");
 });
